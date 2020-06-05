df79179e7a06837d1c536fb828c27616f56f7540
CRSPN <doeraysme@gmail.com>
2020-06-04 19:29:13 -0600

    applied basic open weather data and set to imperial metrics

---
+++ script.js
@@ -0,0 +1,11 @@
+var cityName = "salt lake"
+
+var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=b3617964f38f8a70c001f1c957a1f8ce&units=imperial"
+
+$.ajax({
+    url:currentWeatherUrl,
+    method:"GET"
+}).then(function(data){
+    console.log(data)
+    
+})
