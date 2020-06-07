29779e3dd998d8442e1f5fb64a8b0ca36d06188f
CRSPN <unconfigured@null.spigotmc.org>
2020-06-06 19:43:44 -0700

    added function for displaying a selected citys info

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var cityName = "salt lake"
+var suggestCitys = []
 
 var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=b3617964f38f8a70c001f1c957a1f8ce&units=imperial"
 
@@ -9,3 +10,13 @@ $.ajax({
     console.log(data)
     
 })
+
+var selectCity = function() {
+    localStorage.getItem()
+}
+
+var loadLocalWeather = function() {
+
+    document.getItem
+
+}
